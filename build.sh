53e62ca33ffc96e26f1c8ea71f047054d383d549
Ram Lingala <srilingala1@gmail.com>
2022-04-16 23:05:07 +0100

    updating sonar config

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ git clone git@github.com:rlingala-RP/Gowebapi.git
 
 cd Gowebapi
 
+sonar-scanner.bat -D"sonar.projectKey=psa" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=b2528adf6371b496fced39a8fe8a2ebf0d2b140a"
+
 #build docker image
 docker build -t localhost:8085/gowebapi .
 
