1445ac77bbc7d64458a7458b59c3e7f8dc0a0952
Ram Lingala <srilingala1@gmail.com>
2022-04-20 08:49:13 +0100

    added build.sh

--- build.sh
+++ build.sh
@@ -5,13 +5,12 @@ git clone git@github.com:rlingala-RP/Gowebapi.git
 
 cd Gowebapi
 
-sonar-scanner.bat -D"sonar.projectKey=psa" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=b2528adf6371b496fced39a8fe8a2ebf0d2b140a"
+# submit sonar analysis
+sonar-scanner.bat -D"sonar.projectKey=psa" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=b785fc5d11d15655cb97353a8ae46964043cb677"
 
 #build docker image
 docker build -t localhost:8085/gowebapi .
 
-#docker scan the image
-docker scan localhost:8085/gowebapi
 
 #push docker image to nexus
 docker push localhost:8085/gowebapi
